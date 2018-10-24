91a27bb128908efb2b1ac4c8fe4604c5e98b8834
ShakirIslam <shakir.islam298@gmail.com>
2018-10-25 05:57:06 +1100

    Fully Functional

--- app/src/main/java/com/student/shakirislam/quizapp/WikiSearchActivity.java
+++ app/src/main/java/com/student/shakirislam/quizapp/WikiSearchActivity.java
@@ -5,9 +5,22 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class WikiSearchActivity extends AppCompatActivity {
 
@@ -29,7 +42,8 @@ public class WikiSearchActivity extends AppCompatActivity {
         button_return = (Button) findViewById(R.id.button_back);
         button_detail = (Button) findViewById(R.id.button_details);
         text_wiki_sum = (TextView) findViewById(R.id.text_wiki_summary);
-
+        text_wiki_sum.setText("");
+        getWikiPage(wiki_link);
 
         button_return.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,5 +84,51 @@ public class WikiSearchActivity extends AppCompatActivity {
         WikiSearchActivity.wiki_link = wiki_link;
     }
 
+    private void getWikiPage(String title){
+        //Gets the wikipedia introduction
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + title;
 
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(okhttp3.Call call, Response response) throws IOException {
+                final Response wikiResponse = response;
+
+                WikiSearchActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String data = " ";
+                        try{
+                            data = wikiResponse.body().string();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                        try{
+                            String pagenumber = "";
+                            Iterator <String> iterator = new JSONObject(data).getJSONObject("query").getJSONObject("pages").keys();
+                            while (iterator.hasNext()){
+                                pagenumber = iterator.next();
+                            }
+
+                            data = new JSONObject(data).getJSONObject("query").getJSONObject("pages").getJSONObject(pagenumber).getString("extract");
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                        //Change the textview here
+                        text_wiki_sum.setText(data);
+                    }
+                });
+
+            }
+        });
+    }
 }
